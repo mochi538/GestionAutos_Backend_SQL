f38e389830916b1bae7af838d6205ce55714a6f9
Onixdrew <andrewmosquera1811@gmail.com>
2024-12-10 17:07:18 -0500

    commit

--- controller/clientecontroller.js
+++ controller/clientecontroller.js
@@ -1,34 +1,50 @@
-const bcrypt = require('bcrypt');
-const { Cliente } = require('../models');
+const bcrypt = require("bcrypt");
+const { Cliente } = require("../models");
+const jwt = require('jsonwebtoken');
 
- //login
-exports.loginCliente = async(req, res)=>{
-  try{
-    const {correo, password}= req.body;
-    const cliente = await Cliente.finOne({where:{correo}});
-    if (!cliente ){
-      return res.status(404).json({mjs:"Cliente no encontrado"})
+//login
+exports.loginCliente = async (req, res) => {
+  try {
+    const { correo, password } = req.body;
+    const cliente = await Cliente.findOne({ where: { correo } });
+    if (!cliente) {
+      return res.status(404).json({ mjs: "Cliente no encontrado" });
     }
     const passwordValido = await bcrypt.compare(password, cliente.password);
 
-    if (!passwordValido){
-      return res.status(401).json({mjs:"Contraseña incorrecta"})
+    if (!passwordValido) {
+      return res.status(401).json({ mjs: "Contraseña incorrecta", password });
     }
-  }catch(error){
-    console.error("Error ak iniciara sesion:", error);
-    res.status(500).json({msj:"Error al iniciar sesion", error:error.message})
+    const token = jwt.sign(
+      { id: cliente.id, correo: cliente.correo }, // Payload del token
+      process.env.JWT_SECRET, // Asegúrate de tener una clave secreta en el .env
+      { expiresIn: '1h' } // El token expirará en 1 hora
+    );
+    return res.status(200).json({ msg: "Autenticación exitosa" });
+  } catch (error) {
+    console.error("Error al iniciara sesion:", error);
+    res
+      .status(500)
+      .json({ msj: "Error al iniciar sesion", error: error.message });
   }
 };
-  ////////////////////////////
+////////////////////////////
 exports.registrarCliente = async (req, res) => {
   try {
     const { nombre, correo, numLic, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10); // 10 es el número de salt rounds
-    const nuevoCliente = await Cliente.create({ nombre, correo, numLic, password: hashedPassword });
+    const nuevoCliente = await Cliente.create({
+      nombre,
+      correo,
+      numLic,
+      password: hashedPassword,
+    });
     res.status(201).json(nuevoCliente);
   } catch (error) {
     console.error("Error al crear el cliente:", error);
-    res.status(500).json({ mensaje: "Error al crear el cliente", error: error.message });
+    res
+      .status(500)
+      .json({ mensaje: "Error al crear el cliente", error: error.message });
   }
 };
 
@@ -38,6 +54,8 @@ exports.verClientes = async (req, res) => {
     res.json(clientes);
   } catch (error) {
     console.error("Error al obtener los clientes:", error);
-    res.status(500).json({ mensaje: "Error al obtener los clientes", error: error.message });
+    res
+      .status(500)
+      .json({ mensaje: "Error al obtener los clientes", error: error.message });
   }
 };
--- routes/clienteroutes.js
+++ routes/clienteroutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const clienteController = require('../controller/clientecontroller');
 
 // Ruta para el login de clientes
-
+router.post('/login', clienteController.loginCliente)
 
 // Ruta para registrar un nuevo cliente
 router.post('/registro', clienteController.registrarCliente);
